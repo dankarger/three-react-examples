8fdbfe433c57ce6c23a321c2198d0e9e0dabb2c4
dankarger <karger.dan@gmail.com>
2022-11-22 21:55:56 +0200

    box rotation 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { Canvas, useFrame } from '@react-three/fiber'
 import '../styles/globals.css'
+import MyRotatingBox from "../components/MyRotationBox";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+   <>
+       <Component {...pageProps} />
+
+   </>
+  )
 }
 
 export default MyApp
