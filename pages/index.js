5d5a886affe382f25da2aa65a5c34d59d7bb84a3
dankarger <karger.dan@gmail.com>
2022-11-22 22:09:36 +0200

    two boxes

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import styles from '../styles/Home.module.css'
 import MyRotatingBox from "../components/MyRotationBox";
 
+import TwoSquares from "../components/TwoSquares";
 
 export default function Home() {
   // const myMesh =  useRef()
@@ -22,13 +23,11 @@ export default function Home() {
             <ambientLight intensity={0.1} />
             <directionalLight />
         </Canvas>
+        <h1>second</h1>
+        <TwoSquares />
+        <h3>Third</h3>
     </div>
-         //
-         //
-         // <mesh  ref={myMesh} rotation = {[1,1,1]}>
-         //   <boxGeometry />
-         //   <meshBasicMaterial color="royalblue" />
-         // </mesh>
+
       )
 
 
